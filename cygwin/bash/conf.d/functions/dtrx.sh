fb0f644541237b01db212198905a58e216548ea2
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-06 15:26:31 -0600

    Add function to alias GITHUB/moonpyk:dtrx

---
+++ cygwin/bash/conf.d/functions/dtrx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function dtrx
+  [ -z "$@" ] && ( builtin printf 'No arguments given!' && return 1; )
+
+
+  builtin test -z "$argv";
+    and builtin printf 'No arguments given!';
+    and return 1;
+  repo=${BASH_SOURCE[0]}
+  [ -z "$repo" ] && ( builtin printf 'An error occured while attempting to determine the script directory.\n'; unset repo; builtin return 1 )
+  while [ -f $repo -o -h $repo ]
+  do
+    [ -L $repo ] && ( repo=$(command readlink -f $repo) ) || ( repo=$(command dirname $repo) )
+  done
+  [ -f $(command dirname $repo)/dtrx/scripts/dtrx ] && ( builtin printf 'https://github.com/moonpyk/dtrx not installed!' && return 0; ) || ( $(command dirname $repo)/dtrx/scripts/dtrx -forv --one-entry=inside $argv )
+  unset repo
+end
