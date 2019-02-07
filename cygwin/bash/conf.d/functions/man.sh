9ea2358ff1fcb838e6591aa42f5b4ec2bdab3fbd
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-06 19:01:58 -0600

    Translate decors:fish-colored-man into bash

---
+++ cygwin/bash/conf.d/functions/man.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function man {
+  GROFF_NO_SGR='yes'
+  LESS_TERMCAP_mb=${$man_blink:-$(format bold red)}
+  LESS_TERMCAP_md=${man_bold:-$(format bold)}
+  LESS_TERMCAP_so=${man_standout:-$(format grey)}
+  LESS_TERMCAP_us=${man_underline:-$(format magenta)}
+  LESS_TERMCAP_me=$(builtin printf '\e[0m')
+  LESS_TERMCAP_se=$(builtin printf '\e[0m')
+  LESS_TERMCAP_ue=$(builtin printf '\e[0m')
+  LESS='-R -s'
+
+  [ -z "$MANPATH" ] && ( MANPATH=$(builtin command -v manpath) ) || ( MANPATH=$(echo $MANPATH | tr ' ' ':') )
+
+  command man $argv
+
+}
