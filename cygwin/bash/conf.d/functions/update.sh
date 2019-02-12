1b39c00fe414e48b9cf08a0594f1b313e874b24d
Hunter Richardson <hrichardson8@gmail.com>
2019-02-12 16:24:29 -0600

    Add fundle plugins to cyginw's update.(fi)sh

--- cygwin/bash/conf.d/functions/update.sh
+++ cygwin/bash/conf.d/functions/update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 function update() {
-  fundle self-update && fundle clean && fundle update
-  for i in $(command find ~ -type d -name '.git' | command grep -v 'fundle')
+  for i in $(command find ~ -type d -name '.git')
   do
     command git -C (command dirname $i) pull -- verbose
   done
