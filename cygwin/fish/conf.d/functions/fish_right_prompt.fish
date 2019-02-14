6c8869f84e8dea696477c9a7790da101ed9af5d6
Hunter Richardson <hrichardson8@gmail.com>
2019-02-14 15:58:15 -0600

    Correct errors in fish_right_prompt.fish

--- cygwin/fish/conf.d/functions/fish_right_prompt.fish
+++ cygwin/fish/conf.d/functions/fish_right_prompt.fish
@@ -4,7 +4,7 @@ function fish_right_prompt -d 'the right prompt'
   builtin set -l cmd_performance $CMD_DURATION
   if builtin test $cmd_performance -gt 0
     builtin functions -q humanize_duration;
-      and builtin printf '%s%s  %s ms' $bold $blue (builtin printf '%u' $cmd_performance | humanize_duration);
+      and builtin printf '%s%s  %s' $bold $blue (builtin printf '%u' $cmd_performance | humanize_duration);
       or  builtin printf '%s%s  %u ms' $bold $blue $cmd_performance
   end
   builtin printf ' %s%s%s %s' $bold $cyan (command whoami)
