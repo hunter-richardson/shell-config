aa6129473a05098b18f796fd7b3010e310f03aca
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-14 19:25:27 -0600

    Reload fish on updates to shell

--- cygwin/fish/conf.d/functions/update.fish
+++ cygwin/fish/conf.d/functions/update.fish
@@ -4,6 +4,8 @@ function update -d 'automate software updates with git and fundle'
   for i in (command find ~ -type d -name .git | command grep -v /.config/ | command shuf)
     builtin printf 'Updating %s ...\n' (command git -C $i config --get remote.origin.url);
       and command git -C (command dirname $i) pull --verbose
+    builtin test $i = hunter-richardson/my-config;
+      and source ~/.config/fish/config.fish
   end
   fundle self-update;
     and fundle clean;
