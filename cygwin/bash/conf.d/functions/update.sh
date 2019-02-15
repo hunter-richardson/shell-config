99b3441e588c04a9991601724ffbc0c1e4e3f8cb
Hunter Richardson <hrichardson8@gmail.com>
2019-02-15 17:17:45 -0600

    Break update.sh without Internet connectivity

--- cygwin/bash/conf.d/functions/update.sh
+++ cygwin/bash/conf.d/functions/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function update() {
+  [ ! $(command ping -n 1 github.com >/dev/null) ] && builtin printf 'Unable to open an Internet connection!' && builtin return 0
   for i in $(command find ~ -type d -name '.git' | grep -v '/.config/')
   do
     builtin printf 'Updating %s ...\n' $(command git -C $i config --get remote.origin.url) && command git -C (command dirname $i) pull -- verbose
