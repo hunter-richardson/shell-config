e146e0418cb851b51d4a7c8a94d7d40317623085
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-14 19:33:04 -0600

    Reload bash on updates to shell

--- cygwin/bash/conf.d/functions/update.sh
+++ cygwin/bash/conf.d/functions/update.sh
@@ -4,5 +4,6 @@ function update() {
   for i in $(command find ~ -type d -name '.git' | grep -v '/.config/')
   do
     builtin printf 'Updating %s ...\n' $(command git -C $i config --get remote.origin.url) && command git -C (command dirname $i) pull -- verbose
+    [ $i == 'hunter-richardson/shell-config' ] && source ~/.config/bash/config.sh
   done
 }
