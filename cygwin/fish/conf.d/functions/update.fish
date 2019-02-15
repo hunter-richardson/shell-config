aec17829ed9d0ed6199a03efe0034bb6c5eee667
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-14 19:05:36 -0600

    Update git repos before fundle plugins

--- cygwin/fish/conf.d/functions/update.fish
+++ cygwin/fish/conf.d/functions/update.fish
@@ -1,11 +1,11 @@
 #!/bin/fish
 
 function update -d 'automate software updates with git and fundle'
-  fundle self-update;
-    and fundle clean;
-    and fundle update
   for i in (command find ~ -type d -name .git | command grep -v /.config/ | command shuf)
     builtin printf 'Updating %s ...\n' (command git -C $i config --get remote.origin.url);
       and command git -C (command dirname $i) pull --verbose
   end
+  fundle self-update;
+    and fundle clean;
+    and fundle update
 end
