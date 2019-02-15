d96d84957ef1c180c2c89613a74df562da30df21
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-15 16:40:53 -0600

    Use balias where possible

--- ubuntu/fish/conf.d/functions/fuck.fish
+++ ubuntu/fish/conf.d/functions/fuck.fish
@@ -4,10 +4,13 @@ builtin test (builtin command -v pip3) -a (builtin command pip3 show thefuck);
   and function fuck -d 'integrate fish with https://github.com/nvbn/thefuck'
         set -l TF_PYTHONIOENCODING $PYTHONIOENCODING;
         set -x TF_ALIAS fuck
-        set -x TF_SHELL_ALIASES (alias)
+        set -x TF_SHELL_ALIASES (builtin function -q balias;
+                                   and builtin printf 'balias';
+                                   or  builtin printf 'alias')
         set -x TF_HISTORY (fc -ln -10)
         set -x PYTHONIOENCODING utf-8
-        set -x TF_CMD (thefuck THEFUCK_ARGUMENT_PLACEHOLDER $argv); and eval $TF_CMD
+        set -x TF_CMD (thefuck THEFUCK_ARGUMENT_PLACEHOLDER $argv);
+          and eval $TF_CMD
         set -e TF_HISTORY
         set -x PYTHONIOENCODING $TF_PYTHONIOENCODING
         history $TF_CMD
