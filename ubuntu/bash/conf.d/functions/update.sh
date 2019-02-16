a2667f2e6002b1b2d5c7e59cce8a1f3f06eca932
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-16 13:15:41 -0600

    Update ubuntu:update.sh with Linuxbrew

--- ubuntu/bash/conf.d/functions/update.sh
+++ ubuntu/bash/conf.d/functions/update.sh
@@ -5,19 +5,16 @@ function update() {
     sudo apt-fast update && sudo apt-fast autoremove -y && sudo apt-fast upgrade -y && sudo apt-fast install -fy && sudo apt-fast clean -y
   }
 
+  function __update_brew() {
+    sudo brew update -v && sudo brew upgrade -v
+  }
+
   function __update_git() {
     sudo updatedb
     for i in $(sudo locate -eiq '/.git' | grep -v '/.config/' | command shuf)
     do
       builtin printf 'Updating %s ...\n' $(command git -C $i config --get remote.origin.url) && sudo git -C (command dirname $i) pull --verbose
       [ $i == 'hunter-richardson/shell-config' ] && source /etc/bash/config.sh
-    done
-  }
-
-  function __update_pip() {
-    for i in $(sudo pip3 list --format=freeze | cut -d= -f1 | command shuf)
-    do
-      builtin printf '%s\n' (command whereis $i | command cut -d' ' -f2) && sudo pip3 install $i -U -vvv
     done
   }
 
@@ -42,20 +39,20 @@ function update() {
   for s in $SPMs
   do
     case "$s" in
-                 all)
-                   __update_apt && __update_git && __update_pip && __update_raw && __update_snap;;
-                 apt)
-                   __update_apt;;
-                 git)
-                   __update_git;;
-      (pip|python)3?)
-                   __update_pip;;
-                 raw)
-                   __update_raw;;
-                snap)
-                   __update_snap;;
-                   *)
-                   builtin printf "\a\tUsage:  update [apt | fundle user | git | raw | pip pip3 python python3 | snap | all]\n\tupdate all =:= update apt fundle git pip raw snap\n\tDefault:  update apt git pip raw snap"
+       all)
+         __update_apt && __update_git && __update_pip && __update_raw && __update_snap;;
+       apt)
+         __update_apt;;
+       git)
+         __update_git;;
+      brew)
+         __update_brew;;
+       raw)
+         __update_raw;;
+      snap)
+         __update_snap;;
+         *)
+         builtin printf "\a\tUsage:  update [apt | brew | | git | raw | snap | all]\n\tupdate all =:= update apt brew fundle git raw snap\n\tDefault:  update apt brew git raw snap"
   done
 
   unset -f __update_apt
