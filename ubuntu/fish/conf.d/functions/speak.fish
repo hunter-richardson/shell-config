770f3f20acb68f1ecb91e7e5c6912a604296c8f2
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-17 19:15:14 -0600

    Correct typos in function speak

--- ubuntu/fish/conf.d/functions/speak.fish
+++ ubuntu/fish/conf.d/functions/speak.fish
@@ -2,8 +2,8 @@
 
 function speak
   if builtin command -v spd-say
-    builtin test (builtin command -v fortune) -a (builtin set -q $argv);
-      and command spd-say $argv;
-      or  command spd-say (command fortune)
+    builtin test (builtin command -v fortune) -a ! (builtin set -q $argv);
+      and command spd-say (command fortune)
+      or  command spd-say $argv;
   end
 end
