ed309940835954a9dc449703b9dc18f67a059e57
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-17 19:28:01 -0600

    Function speak: speak given phrase (or random if no arguments given)

---
+++ ubuntu/bash/conf.d/functions/speak.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+function speak() {
+  if [ -n "$(builtin command -v spd-say)" ]
+  then
+    [ -n "$(builtin command -v fortune)" -a -z "$@" ] && ( command spd-say $(command fortune) ) || ( command spd-say $@ )
+  fi
+}
