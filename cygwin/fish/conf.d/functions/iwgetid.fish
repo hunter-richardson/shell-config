de3c7dbd69418dc544287016b1cc9f2b71a4f299
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-20 17:26:11 -0600

    Simplify cygwin:iwgetid.(fi)?sh

--- cygwin/fish/conf.d/functions/iwgetid.fish
+++ cygwin/fish/conf.d/functions/iwgetid.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/fish
 
 function iwgetid -d 'display SSID if connected to the Internet'
-  command powershell 'if(Get-NetConnectionProfile | select Name,IPv4Connectivity | where IPv4Connectivity -eq "Internet") { Write-Host (Get-NetConnectionProfile -IPv4Connectivity "Internet").Name }'
+  command powershell 'if(Get-NetConnectionProfile | select IPv4Connectivity | where IPv4Connectivity -eq "Internet") { Write-Host (Get-NetConnectionProfile -IPv4Connectivity "Internet").Name }'
 end
