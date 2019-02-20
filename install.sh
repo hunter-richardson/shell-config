c84d9687cb633ffc9c146602294a8255d595b6ed
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-19 22:15:16 -0600

    Correct errors in installation

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-[ -z "$1" ] || [ ! -d "$1" ] || [ ! -r "$1" ] || [ ! -w "$1" ] && ( builtin printf 'Please specify a read/writeable working directory.\n'; builtin exit 1 ) || ( conf=$1 )
+[ -z "$1" ] || [ ! -d "$1" ] || [ ! -r "$1" ] || [ ! -w "$1" ] && builtin printf 'Please specify a read/writeable working directory.\n'; builtin exit 1 || conf=$1
 
 repo=$(command find ~ -type d -name 'shell-config')
 
-[ $(command uname -o) == 'Cygwin' ] && ( uname='cygwin' && perms=$(command id -G | command grep -qE '\<554\>') ) || ( uname='ubuntu' && perms=$(sudo -nv ^/dev/null) )
+[ $(command uname -o) == 'Cygwin' ] && uname='cygwin' && perms=$(command id -G | command grep -qE '\<554\>') || uname='ubuntu' && perms=$(sudo -nv ^/dev/null)
 
 for i in "cygwin/git/repos.git" "cygwin/git/config" "$uname/tmux/tmux.conf" "$uname/fish/fish.nanorc" "ubuntu/fish/fish.lang"
   [ ! -f $repo/$i ] && builtin printf 'Please run the %s script in the shell-config local repository directory.\n' $(builtin status filename) && builtin unset perms uname repo conf && builtin exit 1
@@ -27,7 +27,7 @@ fi
 command ln -v $repo/agnostic/bash/conf.d/functions/*.sh $conf/bash/conf.d/functions/
 for i in 'bash' 'bash/conf.d' 'bash/conf.d/functions'
 do
-  [ -d $repo/$uname/$i ] && ( command ln -v $repo/$uname/$i/*.sh $conf/$i/ )
+  [ -d $repo/$uname/$i ] && command ln -v $repo/$uname/$i/*.sh $conf/$i/
 done
 
 if [ -n "$(builtin command -v fish)" ]
@@ -36,7 +36,7 @@ then
   command ln -v $repo/agnostic/fish/conf.d/functions/*.sh $conf/fish/conf.d/functions/
   for i in 'fish' 'fish/conf.d/functions' 'fish/conf.d/completions'
   do
-    [ -d $repo/$uname/$i ] && ( command ln -v $repo/$uname/$i/*.fish $conf/fish/$i/ )
+    [ -d $repo/$uname/$i ] && command ln -v $repo/$uname/$i/*.fish $conf/fish/$i/
   done
   if [ $uname == 'cygwin' ]
   then
@@ -56,10 +56,10 @@ then
   if [ $perms -eq 0 ]
   then
     command ln -v $repo/$uname/fish/fish.nanorc /usr/share/nano/fish.nanorc
-    [ $uname == 'ubuntu' ] && ( command sudo ln -v $repo/$uname/fish/fish.lang /usr/share/gtksourceview-3.0/language-specs/fish.lang )
+    [ $uname == 'ubuntu' ] && command sudo ln -v $repo/$uname/fish/fish.lang /usr/share/gtksourceview-3.0/language-specs/fish.lang
   else
     command ln -v $repo/$uname/fish/fish.nanorc $conf/fish/fish.nanorc && builtin printf 'include %s/fish/fish.nanorc' $conf | command tee -a ~/.nanorc
-    [ $uname == 'ubuntu' ] && ( command ln -v $repo/$uname/fish/fish.lang ${HOME}/.local/share/gtksourceview-3.0/language-specs/fish.lang )
+    [ $uname == 'ubuntu' ] && command ln -v $repo/$uname/fish/fish.lang ${HOME}/.local/share/gtksourceview-3.0/language-specs/fish.lang
   fi
 fi
 
