0bb5c4a549146fc21d44192fbf43049028774854
Hunter Richardson <hrichardson8@gmail.com>
2019-02-20 14:21:06 -0600

    Correct errors in cygwin:dtrx.(fi)?sh

--- cygwin/bash/conf.d/functions/dtrx.sh
+++ cygwin/bash/conf.d/functions/dtrx.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 function dtrx() {
-  [ -z "$*" ] && builtin printf 'Usage: dtrx [options] archive [archive2 ...]\n\ndtrx: error: you did not list any archives' && return 2 || dtrx=$(command find ~ -type f -name 'dtrx' | command grep -v '/.config/') && [ -n "$dtrx" ] && command python $dtrx $* || builtin printf 'https://github.com/moonpyk/dtrx not installed!'
-  builtin unset dtrx
+  if [ -z "$*" ]
+  then
+    builtin printf 'Usage: dtrx [options] archive [archive2 ...]\n\ndtrx: error: you did not list any archives' && return 2
+  else
+    argv=$*
+    [[ $argv =~ *-v* ]] || argv="-v $argv"
+    [[ $argv =~ *--one-entry* ]] || argv="--one-entry inside $argv"
+    dtrx=$(command find ~ -type f -name 'dtrx' | command grep -v '/.config/')
+    [ -n "$dtrx" ] && command python $dtrx $argv || builtin printf 'https://github.com/moonpyk/dtrx not installed!'
+    builtin unset dtrx
+  fi
 }
--- cygwin/fish/conf.d/functions/dtrx.fish
+++ cygwin/fish/conf.d/functions/dtrx.fish
@@ -4,8 +4,13 @@ function dtrx --description 'alias to GITHUB/moonpyk:dtrx'
     and builtin printf 'Usage: dtrx [options] archive [archive2 ...]\n\ndtrx: error: you did not list any archives';
     and return 2
   builtin set dtrx (command find ~ -type f -name 'dtrx')
-  builtin test -n "$dtrx";
-    and command python $dtrx $argv;
-    or  builtin printf '%s not installed!' https://github.com/moonpyk/dtrx
+  if builtin test -n "$dtrx"
+    builtin string match $argv '-v';
+      or builtin set argv -v $argv
+    builtin string match $argv '--one-entry';
+      or builtin set argv --one-entry inside $argv
+    command python $dtrx $argv
+  else
+    builtin printf '%s not installed!' https://github.com/moonpyk/dtrx
   builtin set -e dtrx
 end
