041688f91268e2c65340aec41a33e8dad5505c48
Hunter Richardson <hrichardson8@gmail.com>
2019-02-20 11:10:17 -0600

    Correct errors in cygwin:update.(fi)?sh

--- cygwin/bash/conf.d/functions/update.sh
+++ cygwin/bash/conf.d/functions/update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function update() {
-  [ ! $(command ping -n 1 github.com >/dev/null) ] && builtin printf 'Unable to open an Internet connection!' && builtin return 0
-  for i in $(command find ~ -type d -name '.git' | grep -v '/.config/')
-  do
-    builtin printf 'Updating %s ...\n' $(command git -C $i config --get remote.origin.url) && command git -C (command dirname $i) pull -- verbose
-    [ $i == 'hunter-richardson/shell-config/.git' ] && builtin source ~/.config/bash/config.sh && command tmux source ~/tmux/conf || [ $i == 'tmux-plugins/tpm/.git' ] && command tmux source ~/tmux/conf
-  done
+  if [ $(command ping -n 1 github.com >/dev/null) ]
+  then
+    for i in $(command find ~ -type d -name '.git' | grep -v '/.config/')
+    do
+      builtin printf 'Updating %s ...\n' $(command git -C $i config --get remote.origin.url) && command git -C (command dirname $i) pull -- verbose
+      [ $i == 'hunter-richardson/shell-config/.git' ] && builtin source ~/.config/bash/config.sh && command tmux source ~/tmux/conf || [ $i == 'tmux-plugins/tpm/.git' ] && command tmux source ~/tmux/conf
+    done
+  else
+    builtin printf 'Unable to open an Internet connection!' && builtin return 0
+  fi
 }
--- cygwin/fish/conf.d/functions/update.fish
+++ cygwin/fish/conf.d/functions/update.fish
@@ -1,19 +1,23 @@
 #!/bin/fish
 
 function update -d 'automate software updates with git and fundle'
-  command ping -n 1 github.com >/dev/null;
-    and for i in (command find ~ -type d -name .git | command grep -v /.config/ | command shuf)
-          builtin printf 'Updating %s ...\n' (command git -C $i config --get remote.origin.url);
-            and command git -C (command dirname $i) pull --verbose
-          builtin test $i = hunter-richardson/my-config/.git;
-            and builtin  source ~/.config/fish/config.fish;
-            and command tmux source ~/tmux/conf;
-            or  builtin test $i = tmux-plugins/tpm/.git;
-              and command tmux source ~/tmux/conf;
-        end;
-    and builtin source ~/.config/fish/plugins.fish;
-    and fundle self-update;
-    and fundle clean;
-    and fundle update
-    or  builtin printf 'Unable to open an Internet connection!\n';
+  if command ping -n 1 github.com >/dev/null
+    for i in (command find ~ -type d -name .git | command grep -v /.config/ | command shuf)
+      builtin printf 'Updating %s ...\n' (command git -C $i config --get remote.origin.url);
+        and command git -C (command dirname $i) pull --verbose
+      if builtin test $i = hunter-richardson/my-config/.git
+        builtin  source ~/.config/fish/config.fish;
+          and command tmux source ~/tmux/conf
+      else if builtin test $i = tmux-plugins/tpm/.git
+        command tmux source ~/tmux/conf
+      end
+    end
+    builtin source ~/.config/fish/plugins.fish;
+      and fundle self-update;
+      and fundle clean;
+      and fundle update
+  else
+    builtin printf 'Unable to open an Internet connection!\n';
+    builtin return 0
+  end
 end
