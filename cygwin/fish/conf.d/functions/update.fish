8af1062717c77491ca9a9b9eb4080fd0724e0be4
Hunter Richardson <hrichardson8@gmail.com>
2019-02-27 16:16:59 -0600

    Corrected typo in cygwin:update.fish

--- cygwin/fish/conf.d/functions/update.fish
+++ cygwin/fish/conf.d/functions/update.fish
@@ -15,7 +15,7 @@ function update -d 'automate software updates with git and fundle'
         end
       end
     end
-    builtin source (command find ~ -type f -name plugins.fish | command -v /shell-config/);
+    builtin source (command find ~ -type f -name plugins.fish | command grep -v /shell-config/);
       and fundle self-update;
       and fundle clean;
       and for i in (fundle list --short | command shuf)
