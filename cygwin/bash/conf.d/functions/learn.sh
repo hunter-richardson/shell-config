cd0494804dd8ad1ae9d07be026b44a635557c27a
Hunter Richardson <hrichardson8@gmail.com>
2019-03-11 16:15:34 -0500

    Correct errors in learn.sh

--- cygwin/bash/conf.d/functions/learn.sh
+++ cygwin/bash/conf.d/functions/learn.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function learn {
   [ -n "$1" ] && command curl -k $(builtin printf 'https://cheat.sh/%s/:learn' $1) || command curl -k https://cheat.sh/:list
--- ubuntu/bash/conf.d/functions/learn.sh
+++ ubuntu/bash/conf.d/functions/learn.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function learn {
   [ -n "$1" ] && command curl $(builtin printf 'https://cheat.sh/%s/:learn' $1) || command curl https://cheat.sh/:list
