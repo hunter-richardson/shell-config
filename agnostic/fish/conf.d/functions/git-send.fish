e2a450976ea2db9be016e206c54c03c5ab0a96ce
Hunter Richardson <hrichardson8@gmail.com>
2019-09-26 13:57:25 -0500

    Correct format output in agnostic:git-send.fish

---
+++ agnostic/fish/conf.d/functions/git-send.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/fish
+
+function git-send -d 'Adds, commits, and pushes all changes in the git repository'
+  if builtin test -z "$argv[2]"
+    eval (builtin status current-function) $argv[1] (command pwd)
+  else if builtin test -d $argv[2] -a (command git -C $argv[2] rev-parse --is-inside-work=tree ^/dev/null);
+    command git -C $argv[2] status --porcelain | builtin string length -q;
+      and builtin printf '%sLocal Changes:%s\n' $red $normal;
+      and builtin printf '\t%s\n' (command git -C $argv[2] status --porcelain)
+    command git -C $argv[2] pull --verbose;
+      and command git -C $argv[2] diff --check | builtin string length -q;
+      and builtin printf '%s%sPlease resolve merge conflicts!%s\n' $red $bold $normal;
+      and builtin return 1;
+    command git -C $argv[2] diff | builtin string length -q;
+      and command git -C $argv[2] add --all --renormalize --verbose;
+      and command git -C $argv[2] diff --cached | builtin string length -q;
+      and builtin printf 'Staged changes:\n';
+      and builtin printf '\t%s\n' (command git -C $argv[2] diff --cached);
+    if builtin test -z "$argv[1]"
+      command git -C $argv[2] commit --allow-empty-messages --verbose
+    else
+      command git -C $argv[2] commit --verbose --message="$argv[1]"
+    end;
+      and command git -C $argv[2] push --dry-run --verbose
+  else
+    builtin printf 'fatal: not a git repository (or any of the parent directories): %s' $argv[2]
+  end
+end
