7da377a3d84f859dd7063a0558dbc2f0267ea814
Hunter Richardson <hrichardson8@gmail.com>
2019-09-26 13:58:13 -0500

    Correct format output in agnostic:git-send.fish

--- agnostic/fish/conf.d/functions/git-send.fish
+++ agnostic/fish/conf.d/functions/git-send.fish
@@ -12,7 +12,7 @@ function git-send -d 'Adds, commits, and pushes all changes in the git repositor
       and builtin printf '%s%sPlease resolve merge conflicts!%s\n' $red $bold $normal;
       and builtin return 1;
     command git -C $argv[2] diff | builtin string length -q;
-      and command git -C $argv[2] add --all --renormalize --verbose;
+      and command git -C $argv[2] add --all --renormalize;
       and command git -C $argv[2] diff --cached | builtin string length -q;
       and builtin printf 'Staged changes:\n';
       and builtin printf '\t%s\n' (command git -C $argv[2] diff --cached);
