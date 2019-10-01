60c45297aa7411c41d83347ad63859dae3e3e5bd
Hunter Richardson <hrichardson8@gmail.com>
2019-10-01 09:02:26 -0500

    Correct errors in ubuntu:git-send, move agnostic:git-send to cygwin

---
+++ ubuntu/bash/conf.d/functions/git-send.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function git-send() {
+  if [ -z "$2" ]
+  then
+    builtin eval $0 $1 $(command pwd)
+  else if [ -d $2 -a $(command git -C $2 rev-parse --is-inside-work=tree ^/dev/null) ]
+    builtin set url $(command git -C $2 config --get remote.origin.url) && builtin printf 'REPOSITORY: %s/%s\n' $(builtin printf '%s' $url | command cut -d/ -f3 | command cut -d. -f1 | command tr [a-z] [A-Z]) $(builtin printf '%s' $url | command cut -d/ -f4,5 | command sed -e 's/\//:/g' | command sed -e 's/hunter-richardson/\$ME/g')
+    [ $(command git -C $2 status --porcelain | command wc -c) -ne 0 ] && builtin printf '%sLocal Changes:%s\n' $(format red) $(format normal) && builtin printf '\t%s\n' $(command git -C $2 status --porcelain) && builtin printf '\n'
+    sudo git -C $2 pull --verbose
+    [ $(command git -C $2 diff --check | command wc -c) -ne 0 ] && builtlin printf '\n%s%sPlease resolve merge conflicts!%s\n' $(format red bold) $(format normal) && builtlin return 1
+    if [ $(command git -C $2 diff | command wc -c) -ne 0 ]
+    then
+      sudo git -C $2 add --all --renormalize
+      [ $(command git -C $2 diff --cached | command wc -c) -ne 0 ] && builtin printf '\n%sStaged Changes:%s\n' $(format red) $(format normal) && builtin printf '\t%s\n' $(command git -C $2 diff --cached) && builtin printf '\n'
+    fi
+    if [ builtin test -z "$1" ]
+    then
+      sudo git -C $2 commit --allow-empty-message --verbose && sudo git -C $2 push --verbose
+    else
+      sudo git -C $2 commit --verbose --message="$1" && sudo git -C $2 push --verbose
+    fi
+  else
+    builtin printf 'fatal: not a git repository' $2
+  fi
+}
---
+++ ubuntu/fish/conf.d/functions/git-send.fish
@@ -0,0 +1,32 @@
+#!/usr/bin/fish
+
+function git-send -d 'Adds, commits, and pushes all changes in the git repository'
+  if builtin test -z "$argv[2]"
+    eval (builtin status current-function) $argv[1] (command pwd)
+  else if builtin test -d $argv[2] -a (command git -C $argv[2] rev-parse --is-inside-work=tree ^/dev/null);
+    builtin set -l url (command git -C $argv[2] config --get remote.origin.url);
+      and builtin printf 'REPOSITORY: %s/%s\n' (builtin printf '%s' $url | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper) (builtin printf '%s' $url | command cut -d/ -f4,5 | builtin string replace / : | builtin string replace hunter-richardson \$ME)
+    command git -C $argv[2] status --porcelain | builtin string length -q;
+      and builtin printf '%sLocal Changes:%s\n' $red $normal;
+      and builtin printf '\t%s\n' (command git -C $argv[2] status --porcelain);
+      and builtin printf '\n'
+    sudo git -C $argv[2] pull --verbose;
+      and command git -C $argv[2] diff --check | builtin string length -q;
+      and builtin printf '\n%s%sPlease resolve merge conflicts!%s\n' $red $bold $normal;
+      and builtin return 1;
+    command git -C $argv[2] diff | builtin string length -q;
+      and sudo git -C $argv[2] add --all --renormalize;
+      and command git -C $argv[2] diff --cached | builtin string length -q;
+      and builtin printf '\n%sStaged Changes:%s\n' $red $normal;
+      and builtin printf '\t%s\n' (command git -C $argv[2] diff --cached);
+      and builtin printf '\n';
+    if builtin test -z "$argv[1]"
+      sudo git -C $argv[2] commit --allow-empty-message --verbose
+    else
+      sudo git -C $argv[2] commit --verbose --message="$argv[1]"
+    end;
+      and sudo git -C $argv[2] push --verbose
+  else
+    builtin printf 'fatal: not a git repository: %s' $argv[2]
+  end
+end
