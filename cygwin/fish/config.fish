540cc4723cbe7fa1b5f9409d931f8236e1d415c9
Hunter Richardson <hrichardson8@gmail.com>
2019-10-02 00:11:17 -0500

    Allow the world to read fundle.plugins; colorize config.fish output

--- cygwin/fish/config.fish
+++ cygwin/fish/config.fish
@@ -18,35 +18,37 @@ builtin set --local MY_DIR (command dirname (builtin status filename))
 
 for i in functions completions
   builtin source $MY_DIR/conf.d/$i/fundle.fish;
-    and builtin printf 'load GITHUB/danhper:fundle fundle %s\n' (builtin string replace s '' $i)
+    and builtin printf 'load %s%sGITHUB/%sdanhper:fundle%s fundle %s\n' $bold $blue $red $normal (builtin string replace s '' $i)
 end
 
 for i in (command grep -Ev '^#' (command find -type f -name fundle.plugins | command grep -v /git/) | command shuf);
-  fundle plugin $i;
-    and builtin printf 'load plugin %s/%s by fundle\n' (builtin printf '%s' $__fundle_plugin_urls | command grep $i | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper) (builtin string replace / : $i | builtin string replace hunter-richardson \$ME)
+  builtin set -l src (builtin printf '%s' $__fundle_plugin_urls | command grep $i | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper);
+    and fundle plugin $i;
+    and builtin printf 'load plugin %s%s%s/%s%s%s by fundle\n' $bold $blue $src $red (builtin string replace / : $i | builtin string replace hunter-richardson \$ME) $normal
 end;
   and fundle init
 
 for i in (fundle list --short | command shuf)
-  for d in functions completions
-    builtin test -d $MY_DIR/fundle/$i/$d;
-      and for f in (command ls -1 $MY_DIR/fundle/$i/$d/*.fish | command shuf)
-            builtin source $f;
-              and builtin printf 'load plugin %s/%s %s %s\n' (builtin printf '%s' $__fundle_plugin_urls | command grep $i | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper) (builtin printf '%s' $i | builtin string replace / : | builtin string replace hunter-richardson \$ME) (command basename $f .fish) (builtin string replace s '' $d)
-          end
-  end
+  builtin set -l src (builtin printf '%s' $__fundle_plugin_urls | command grep $i | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper);
+    and for d in functions completions
+          builtin test -d $MY_DIR/fundle/$i/$d;
+            and for f in (command ls -1 $MY_DIR/fundle/$i/$d/*.fish | command shuf)
+                  builtin source $f;
+                    and builtin printf 'load plugin %s%s%s/%s%s%s %s %s\n' $bold $blue $src $red (builtin string replace / : $i | builtin string replace hunter-richardson \$ME) $normal (command basename $f .fish) (builtin string replace s '' $d)
+                end
+        end
 end
 
 for i in functions completions
   for f in (command ls -1 $MY_DIR/conf.d/$i/*.fish | builtin string match -vr 'fundle.fish$' | command shuf)
     builtin source $f;
-      and builtin printf 'load GITHUB/$ME:shell-config %s %s\n' (command basename $f .fish) (builtin string replace s '' $i)
+      and builtin printf 'load %s%sGITHUB/%s$ME:shell-config%s %s %s\n' $bold $blue $red (command basename $f .fish) (builtin string replace s '' $i) $normal
   end
 end
 
 for i in (command ls -1 $MY_DIR/conf.d/*.fish | command shuf)
   builtin source $i;
-    and builtin printf 'load GITHUB/$ME:shell-config %s\n' (
+    and builtin printf 'load %s%sGITHUB/%s$ME:shell-config%s %s\n' $bold $blue $red $normal (
       builtin string match -iqr 'abbrs' (command basename $i .fish);
         and builtin printf 'abbreviations';
         or  builtin printf 'exported variables')
@@ -54,5 +56,5 @@ end
 
 builtin test -f $MY_DIR/alias.fish;
   and builtin source $MY_DIR/alias.fish;
-  and builtin printf 'load GITHUB/$ME:shell-config aliases\n';
+  and builtin printf 'load %s%sGITHUB/%s$ME:shell-config%s aliases\n' $bold $blue $red $normal;
   or  true
--- ubuntu/fish/config.fish
+++ ubuntu/fish/config.fish
@@ -16,22 +16,14 @@
 
 set --local MY_DIR (command dirname (builtin status filename))
 
-builtin source /root/.config/fish/conf.d/functions/fundle.fish;
-  and for i in (command grep -Ev '^#' /root/.config/fish/fundle.plugins | command shuf)
-        fundle plugin $i;
-          and builtin printf 'load plugin %s/%s by fundle\n' (builtin printf '%s' $__fundle_plugin_urls | builtin string match $i | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper) (builtin string replace / : $i | builtin string replace hunter-richardson \$ME)
-      end;
-  and fundle init;
-  and functions -e fundle
-
 for i in functions completions
   builtin test -d $MY_DIR/conf.d/$i;
     and for f in (command ls -1 $MY_DIR/conf.d/$i/*.fish | command shuf)
           builtin source $f;
             and builtin test (command whoami) != root;
             and builtin printf 'load %s\n' (
-              contains_seq (command basename $i .fish) fish_prompt fish_right_prompt fuck gclone learn open refish speak update youtube-dl;
-                and builtin printf 'GITHUB/$ME:shell-config %s %s' (command basename $f .fish) (builtin string replace s '' $i);
+              builtin contains (command basename $f) (command ls -1 /usr/share/git/repos/shell-config/{agnostic,ubuntu}/fish/conf.d/{comple,func}tions/*.fish  | command xargs -n 1 basename);
+                and builtin printf '%s%sGITHUB/%s$ME:shell-config%s %s %s' $bold $blue $red $normal (command basename $f .fish) (builtin string replace s '' $i);
                 or  builtin printf '%s %s from fundle plugin' (builtin string replace s '' $i) (command basename $f .fish));
             or  true
         end
@@ -40,19 +32,22 @@ end
 for f in (command ls -1 $MY_DIR/conf.d/*.fish | command shuf)
   builtin source $f;
     and builtin test (command whoami) != root;
-    and builtin printf 'source %s\n' $f;
+    and builtin printf 'load %s%sGITHUB/%s$ME:shell-config%s %s\n' $bold $blue $red $normal (
+      builtin test (command basename $f .fish) = abbrs;
+        and builtin printf 'abbreviations';
+        or  builtin printf 'exported variables');
     or  true
 end
 
 builtin test -f $MY_DIR/alias.fish;
   and builtin source $MY_DIR/alias.fish;
   and builtin test (command whoami) != root;
-  and builtin printf 'source %s/alias.fish\n' $MY_DIR;
+  and builtin printf 'load %s%sGITHUB/%s$ME:shell-config%s aliases\n' $bold $blue $red $normal;
   or  true
 
-builtin command -v albert >/dev/null;
+builtin test (command whoami) != root;
+  and builtin command -v albert >/dev/null;
   and builtin bind \e\e 'command albert show 1>/dev/null 2>/dev/null';
-  and builtin test (command whoami) != root;
   and builtin printf 'bind ESC-ESC albert\n';
   or  true
 
