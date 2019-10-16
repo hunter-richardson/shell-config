c7965498bb2444d0bf32c57bfac6e0e9ec55b86f
Hunter Richardson <hrichardson8@gmail.com>
2019-10-16 12:34:52 -0500

    Corrected color output in agnostic:retry

--- agnostic/bash/conf.d/functions/retry.sh
+++ agnostic/bash/conf.d/functions/retry.sh
@@ -10,15 +10,15 @@ function retry()
     if [ $count -lt $retries ]
     then
       builtin local wait=$((2 ** $count))
-      builtin printf 'Try %s%d%s of %d%s exited %s%d%s, retrying in %s%d%s %s...\n' $(format bold green) $i $(format blue) $retries $(format normal) $(format bold red) $? $(format normal) $(format yellow) $wait $(format normal) $([ $wait -gt 1 ] && builtin printf 'seconds' || builtin printf 'second ')
+      builtin printf 'Try %s%d%s of %s%d%s exited %s%d%s, retrying in %s%d%s %s...\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal) $(format bold red) $? $(format normal) $(format yellow) $wait $(format normal) $([ $wait -gt 1 ] && builtin printf 'seconds' || builtin printf 'second ')
       command sleep $wait
     else
       builtin local exit=$?
-      builtin printf 'Try %s%d%s of %d%s exited %s%d%s.\n' $(format bold green) $i $(format blue) $retries $(format normal) $(format bold red) $exit $(format normal)
+      builtin printf 'Try %s%d%s of %s%d%s exited %s%d%s.\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal) $(format bold red) $exit $(format normal)
       builtin return $exit
     fi
   done
-  buitlin printf 'Try %s%d%s of %d%s succeeded.\n' $(format bold green) $i $(format blue) $retries $(format normal)
+  buitlin printf 'Try %s%d%s of %s%d%s succeeded.\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal)
   builtin return 0
 }
 
--- agnostic/fish/conf.d/functions/retry.fish
+++ agnostic/fish/conf.d/functions/retry.fish
@@ -6,15 +6,15 @@ function retry -d 'Retry a command up to a specified number of times, until it e
   builtin set -l command $argv;
   for i in (command seq $retries)
     if eval $command
-      builtin printf 'Try %s%s%d%s of %d%s succeeded.\n' $bold $green $i $blue $retries $normal
+      builtin printf 'Try %s%s%d%s of %s%s%d%s succeeded.\n' $bold $green $i $normal $bold $blue $retries $normal
       builtin return 0
     else if builtin test $i -eq $retries
       builtin set -l exit $status
-      builtin printf 'Try %s%s%d%s of %d%s exited %s%s%d%s.\n' $bold $green $i $blue $retries $normal $bold $red $exit $normal
+      builtin printf 'Try %s%s%d%s of %s%s%d%s exited %s%s%d%s.\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $exit $normal
       builtin return $exit
     else
       builtin set -l wait (math "2 ^ ($i - 1)")
-      builtin printf 'Try %s%s%d%s of %d%s exited %s%s%d%s, retrying in %s%s%d%s %s...\n' $bold $green $i $blue $retries $normal $bold $red $status $normal $bold $yellow $wait $normal (
+      builtin printf 'Try %s%s%d%s of %s%s%d%s exited %s%s%d%s, retrying in %s%s%d%s %s...\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $status $normal $bold $yellow $wait $normal (
         builtin test $wait -gt 1;
           and builtin printf 'seconds';
           or  builtin printf 'second ')
