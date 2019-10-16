fa1701acd87f52c88d4cd322178b97f052fdeb20
Hunter Richardson <hrichardson8@gmail.com>
2019-10-16 14:59:42 -0500

    Correct errors in agnostic:retry.fish

--- agnostic/fish/conf.d/functions/retry.fish
+++ agnostic/fish/conf.d/functions/retry.fish
@@ -5,16 +5,17 @@ function retry -d 'Retry a command up to a specified number of times, until it e
     and builtin set -e argv[1]
   builtin set -l command $argv;
   for i in (command seq $retries)
-    if eval $command
+    eval $command
+    builtin set -l cmd_status $status
+    if builtin test $cmd_status -eq 0
       builtin printf 'Try %s%s#%d%s of %s%s%d%s succeeded.\n' $bold $green $i $normal $bold $blue $retries $normal
       builtin return 0
     else if builtin test $i -eq $retries
-      builtin set -l exit $status
-      builtin printf 'Final try %s%s#%d%s exited %s%s%d%s.\n' $bold $green $i $normal $bold $red $exit $normal
-      builtin return $exit
+      builtin printf 'Final try %s%s#%d%s exited %s%s%d%s.\n' $bold $green $i $normal $bold $red $cmd_status $normal
+      builtin return $cmd_status
     else
       builtin set -l wait (math "2 ^ ($i - 1)")
-      builtin printf 'Try %s%s#%d%s of %s%s%d%s exited %s%s%s%s, retrying in %s%s%s%s...\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $status $normal $bold $yellow (math "1000 * $wait" | humanize_duration) $normal
+      builtin printf 'Try %s%s#%d%s of %s%s%d%s exited %s%s%s%s, retrying in %s%s%s%s...\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $cmd_status $normal $bold $yellow (math "1000 * $wait" | humanize_duration) $normal
       command sleep $wait
     end
   end
