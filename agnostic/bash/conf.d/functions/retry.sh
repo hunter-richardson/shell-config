9aade15b26b2422caaacad0dbc1c63cf112207f4
Hunter Richardson <hrichardson8@gmail.com>
2019-10-16 14:38:36 -0500

    Translate humaize_duration to bash; humanize waittimes in agnostic:retry.sh

--- agnostic/bash/conf.d/functions/retry.sh
+++ agnostic/bash/conf.d/functions/retry.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/fish
+#!/bin/bash
 
 function retry()
 {
@@ -7,18 +7,18 @@ function retry()
   builtin shift
   until "$@"
   do
-    if [ $count -lt $retries ]
+    if [ $count -eq $retries ]
     then
-      builtin local wait=$((2 ** $count))
-      builtin printf 'Try %s%d%s of %s%d%s exited %s%d%s, retrying in %s%d%s %s...\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal) $(format bold red) $? $(format normal) $(format yellow) $wait $(format normal) $([ $wait -gt 1 ] && builtin printf 'seconds' || builtin printf 'second ')
-      command sleep $wait
-    else
       builtin local exit=$?
-      builtin printf 'Try %s%d%s of %s%d%s exited %s%d%s.\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal) $(format bold red) $exit $(format normal)
+      builtin printf 'Final try #%s#%d%s exited %s%d%s.\n' $(format bold green) $i $(format normal) $(format bold red) $exit $(format normal)
       builtin return $exit
+    else
+      builtin local wait=$((2 ** $count))
+      builtin printf 'Try %s%d%s of %s#%d%s exited %s%d%s, retrying in %s%s%s...\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal) $(format bold red) $? $(format normal) $(format bold yellow) $(command expr 1000 \* $wait | humanize_duration) $(format normal)
+      command sleep $wait
     fi
   done
-  buitlin printf 'Try %s%d%s of %s%d%s succeeded.\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal)
+  buitlin printf 'Try %s%d%s of %s#%d%s succeeded.\n' $(format bold green) $i $(format normal) $(format bold blue) $retries $(format normal)
   builtin return 0
 }
 
--- agnostic/fish/conf.d/functions/retry.fish
+++ agnostic/fish/conf.d/functions/retry.fish
@@ -6,15 +6,15 @@ function retry -d 'Retry a command up to a specified number of times, until it e
   builtin set -l command $argv;
   for i in (command seq $retries)
     if eval $command
-      builtin printf 'Try %s%s%d%s of %s%s%d%s succeeded.\n' $bold $green $i $normal $bold $blue $retries $normal
+      builtin printf 'Try %s%s#%d%s of %s%s%d%s succeeded.\n' $bold $green $i $normal $bold $blue $retries $normal
       builtin return 0
     else if builtin test $i -eq $retries
       builtin set -l exit $status
-      builtin printf 'Try %s%s%d%s of %s%s%d%s exited %s%s%d%s.\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $exit $normal
+      builtin printf 'Final try %s%s#%d%s exited %s%s%d%s.\n' $bold $green $i $normal $bold $red $exit $normal
       builtin return $exit
     else
       builtin set -l wait (math "2 ^ ($i - 1)")
-      builtin printf 'Try %s%s%d%s of %s%s%d%s exited %s%s%s%s, retrying in %s%s%s%s...\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $status $normal $bold $yellow (math "1000 * $wait" | humanize_duration) $normal
+      builtin printf 'Try %s%s#%d%s of %s%s%d%s exited %s%s%s%s, retrying in %s%s%s%s...\n' $bold $green $i $normal $bold $blue $retries $normal $bold $red $status $normal $bold $yellow (math "1000 * $wait" | humanize_duration) $normal
       command sleep $wait
     end
   end
