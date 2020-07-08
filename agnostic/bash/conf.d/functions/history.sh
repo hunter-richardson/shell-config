694373cb4bdf404e898b96fe2bbacd60b5af46bb
Hunter Richardson <hrichardson8@gmail.com>
2020-07-08 12:27:47 -0500

    Correct error in redhat:history.sh, rewrite redhat:rm.sh

--- agnostic/bash/conf.d/functions/history.sh
+++ agnostic/bash/conf.d/functions/history.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-[ ! -s $HOME/.bash_history ] && builtin printf '%sNo %s history!%s\n' $(format bold) $SHELL $(format normal) && builtin return 0
-[ $# -eq 0 ] && command less $HOME/.bash_history && builtin printf '%s history may be filtered by:\t %s [string query]\n' $SHELL $0 && builtin return 0
-command grep "$*" $HOME/.bash_history --color=always | command grep -v "$HOME/.bash_history"
+function history {
+  [ ! -s $HOME/.bash_history ] && builtin printf '%sNo %s history!%s\n' $(format bold) $SHELL $(format normal) && builtin return 0
+  [ $# -eq 0 ] && command less $HOME/.bash_history && builtin printf '%s history may be filtered by:\t history [string query]\n' $(command basename $SHELL) && builtin return 0
+  command grep "$*" $HOME/.bash_history --color=always | command grep -v "$HOME/.bash_history"
+}
--- redhat/bash/conf.d/functions/rm.sh
+++ redhat/bash/conf.d/functions/rm.sh
@@ -4,22 +4,11 @@ function rm {
   [ $# -eq 0 ] && builtin return 0 || builtin local retval=0
   for i in $*
   do
-    if [ -d "$i/.git/objects" ] && [ -w "$i/.git/objects" ]
-    then
-      command shred -fuvxz --remove=unlink --iterations=1 "$i/.git/objects/*/*"
-      command rm -dv "$i/.git/objects/*" "$i/.git/objects"
-      rm $i
-    elif [ -d "$i" ] && [ -w "$i" ]
-    then
-      builtin printf 'Inspecting directory %s\n' "$i"
-      [ $(command ls "$i" | command wc -w) -gt 0 ] && rm "$i/*"
-      command rm -dv "$i"
-    elif [ -f "$i" ] && [ -w "$i" ]
-    then
-      command shred -fuvxz --remove=unlink --iterations=1 "$i"
-    else
-      builtin printf 'Cannot delete %s%s%s!\n' $(format bold red) $i $(format normal) && retval=2
-    fi
+    for f in $(command find $i -type f)
+    do
+      command shred -fuvxz --remove=unlink --iterations=1 "$f"
+    done
+    [ $? -eq 0 ] && command rm -drv "$i"
   done
-  builtin return $retval
 }
+
