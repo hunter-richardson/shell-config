feb036f005a2fd46bb473260184bef61ae936b7a
Hunter Richardson <hrichardson8@gmail.com>
2020-07-09 12:44:54 -0500

    redhat:rm.sh special cases

--- redhat/bash/conf.d/functions/rm.sh
+++ redhat/bash/conf.d/functions/rm.sh
@@ -4,11 +4,25 @@ function rm {
   [ $# -eq 0 ] && builtin return 0 || builtin local retval=0
   for i in $*
   do
-    for f in $(command find $i -type f)
-    do
-      command shred -fuvxz --remove=unlink --iterations=1 "$f"
-    done
-    [ $? -eq 0 ] && command rm -drv "$i"
+    if [ -a "$i" ]
+    then
+      command chmod -cR u+w $i
+      if [ -d $i ]
+      then
+        for f in $(command find $i -type f)
+        do
+          command shred -fuvxz --remove=unlink --iterations=1 $f
+        done
+        [ $? -eq 0 ] && command rm -drv $i
+      elif [ -f $i ]
+      then
+        command shred -fuvxz --remove=unlink --iterations=1 $i
+      else
+        command rm -fv $i
+      fi
+    else
+      builtin printf '%s: No such file or directory' $i
+    fi
   done
 }
 
