af6fee44cbfc0ba762755eb6be510a2bb2f356b9
Hunter Richardson <hrichardson8@gmail.com>
2021-02-22 17:40:43 -0600

    Correct error in redhat:update.sh

--- redhat/bash/conf.d/functions/update.sh
+++ redhat/bash/conf.d/functions/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 update() {
-  for i in $(command find ~ -type d -name '.git')
+  for i in $(command find ~ -type d -name '.git' 2>&1 | command grep -v 'Permission denied')
   do
     builtin printf '%s\n' $i
     builtin cd $i/..
